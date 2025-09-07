5add0ca1d98a0de1afaec90a29368536d2c02cd3
Preston Yoshino <prestonyoshino@Prestons-Air.grinnell.edu>
2025-09-07 14:01:28 -0500

    done

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -41,7 +41,7 @@ const HomePage = () => {
     {isRateLimited && <RateLimitedUI />}
 
     <div className='max-w-7xl mx-auto p-4 mt-6'>
-      {loading && <div className='text-center text-primary py-10'>Loding...</div>}
+      {loading && <div className='text-center text-primary py-10'>Loading...</div>}
       {notes.length === 0 && !isRateLimited && <NotesNotFound />}
       {notes.length > 0 && !isRateLimited &&  (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
